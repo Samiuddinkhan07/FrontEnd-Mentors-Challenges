cce75b4ccbd038fda19bf932c17ac68f4a76f71c
OneCoder <samiuddinkhan2000@gmail.com>
2023-11-23 16:01:48 +0530

    finalized the template

--- news-homepage-main/script.js
+++ news-homepage-main/script.js
@@ -1,4 +1,5 @@
-
+const menuIcon = document.getElementById("menu-icon");
+const closeIcon = document.getElementById("close-icon");
 
 const changeImageOnPhone = () =>{
     const img = document.getElementById('desktop-view');
@@ -13,5 +14,21 @@ const changeImageOnPhone = () =>{
     }
 }
 
-addEventListener("resize",changeImageOnPhone)
-addEventListener("load",changeImageOnPhone)
+addEventListener("resize",changeImageOnPhone);
+addEventListener("load",changeImageOnPhone);
+
+const toggleNavMenu = () =>{
+    const mobileMenu = document.getElementById("mobile-menu");
+    const overlay = document.getElementById("bg-overlay");
+    if(mobileMenu.style.display == "none" || mobileMenu.style.display == ""){
+        mobileMenu.style.display = "block"
+        overlay.style.display = "block"
+    }
+    else{
+        mobileMenu.style.display = "none";
+        overlay.style.display = "none"
+    }
+}
+
+menuIcon.addEventListener('click',toggleNavMenu);
+closeIcon.addEventListener('click',toggleNavMenu)
